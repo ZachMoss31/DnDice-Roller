d69c32db1039627b5946ba27d54aa964e2c4b1b7
Ganon4Prez <88854718+ZachMoss31@users.noreply.github.com>
2024-01-19 01:40:54 -0700

    Created first window and first button via PySide for project.

--- DnDiceRoller/Application.py
+++ DnDiceRoller/Application.py
@@ -1,7 +1,34 @@
 #Application driver
 from Lib.dicelib import *
 
+#UI Libs
+from PySide6.QtCore import QSize, Qt
+from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow
+
+#System for command line use
+
+#Best main window use is to subclass it
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        self.setWindowTitle("DnDice")
+        self.setFixedSize(400,300)
+        button = QPushButton("Roll Dice")
+        self.setCentralWidget(button)
+    
+
 def main():
+    
+    app = QApplication([])
+
+    #window = QWidget()
+    window = MainWindow()
+    window.show()
+    
+    #Event Loop
+    app.exec()
+    
     #Preprocessing...
     print("Enter a number to roll, or {q} to quit and close.")
     choice = ""
